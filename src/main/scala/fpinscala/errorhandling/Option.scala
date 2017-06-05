d790a90d92193c59dbec1200e9fb094751e523b0
pluselc <ij4nu8d4fw@gmail.com>
2017-06-06 00:12:56 +0900

    add: exercise 4-1

---
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -0,0 +1,48 @@
+package fpinscala.errorhandling
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+//    case Some(a) => Some(a)
+    case Some(_) => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+//    case Some(a) if f(a) => Some(a)
+    case Some(a) if f(a) => this
+    case Some(_) => None
+  }
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def failingFn(i: Int): Int = {
+    val y: Int = throw new Exception("fail!")
+    try {
+      val x = 42 + 5
+      x + y
+    } catch {
+      case e: Exception => 43
+    }
+  }
+}
